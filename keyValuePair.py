439d45ba2e9c2c05255d20ba675b101640343a39
Eren Çolak <erenncolak@hotmail.com>
2022-11-07 17:55:21 +0300

    Key-Value Pair code added

---
+++ keyValuePair.py
@@ -0,0 +1,35 @@
+a = 0
+b=0
+inputnumber = int(input())
+sozluk={}
+while a < inputnumber:
+    a=a+1
+    nameAndNumber=str(input())
+    splitList=nameAndNumber.split()
+    sozluk[splitList[0]]=splitList[1]
+while b < inputnumber:
+    b=b+1
+    names=str(input())
+    if sozluk.get(names,"Not found") == "Not found":
+        print(sozluk.get(names,"Not found") , end="\n",sep="")
+    else:
+        print(names,"="+sozluk.get(names,"Not found") , end="\n",sep="")
+        
+'''
+inputs
+3
+sam 99912222
+tom 11122222
+harry 12299933
+sam
+edward
+harry
+'''   
+
+'''
+Expected Output
+sam=99912222
+Not found
+harry=12299933
+''' 
+    
